c8b037019e67f593b160c88a9184a1480d72e3ff
sundholmm <oskar.sundholm@protonmail.com>
2020-08-16 19:26:33 +0300

    added assets to views

--- components/DrinkVoucherUsedView/DrinkVoucherUsedView.js
+++ components/DrinkVoucherUsedView/DrinkVoucherUsedView.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-export default DrinkVoucherUsedView = () => {
+const DrinkVoucherUsedView = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={require('../../assets/heidis-voucher-background.jpeg')} style={styles.backgroundImage} >
+        <Text style={styles.voucherTitle}>Drink voucher</Text>
+        <Text style={styles.voucherSubTitle}>Heidi's Bier Bar Turku</Text>
+        <Image source={require('../../assets/heidis-voucher.jpeg')} style={styles.voucherLogo}/>
+      <View style={styles.iconBorder}>
+        <FontAwesomeIcon style={styles.icon} icon={ faCheck } />
+      </View>
+      <Text style={styles.text}>Perk used successfully!</Text>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.dismissBorder}>Dismiss</Text>
+        <Text style={styles.useAgainBorder}>Use again</Text>
+      </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -17,4 +30,71 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  voucherLogo: {
+    marginTop: '25px',
+    width: '150px',
+    height: '150px'
+  },
+  voucherTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: '20px'
+  },
+  voucherSubTitle: {
+    color: 'grey',
+    fontSize: '17px',
+    opacity: 0.75
+  },
+  backgroundImage: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    resizeMode: 'cover', // or 'stretch'
+  },
+  iconBorder: {
+    margin: 16,
+    padding: 8,
+    borderWidth: 4,
+    borderColor: "#00FA9A",
+    borderRadius: 50,
+    color: '#00FA9A',
+  },
+  icon: {
+    color: '#00FA9A',
+  },
+  text: {
+    color: '#00FF00',
+    fontWeight: "bold"
+  },
+  buttonContainer: {
+    marginTop: '50px',
+    flexDirection: 'row',
+    maxHeight: 200
+  },
+  dismissBorder: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "20px",
+    borderColor: '#DC143C',
+    borderWidth: 4,
+    borderRadius: '50%',
+    width: 120,
+    lineHeight: 120,
+    textAlign: 'center',
+    margin: 20
+  },
+  useAgainBorder: {
+    color: "#C0C0C0",
+    fontWeight: "bold",
+    fontSize: "20px",
+    borderColor: '#C0C0C0',
+    borderWidth: 4,
+    borderRadius: '50%',
+    width: 120,
+    lineHeight: 120,
+    textAlign: 'center',
+    margin: 20
+  }
 });
+
+export default DrinkVoucherUsedView;
