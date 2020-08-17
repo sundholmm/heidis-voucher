588806a271f70dd5b23f2673b6c1fc58aeaa83bf
sundholmm <oskar.sundholm@protonmail.com>
2020-08-17 22:19:16 +0300

    added statusbar styling

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import DrinkVoucherUsedView from './components/DrinkVoucherUsedView/DrinkVoucherUsedView';
-import DrinkVoucherView from './components/DrinkVoucherView/DrinkVoucherView';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import DrinkVoucherUsedView from "./components/DrinkVoucherUsedView/DrinkVoucherUsedView";
+import DrinkVoucherView from "./components/DrinkVoucherView/DrinkVoucherView";
 
 const App = () => {
   return (
     <View style={styles.container}>
       <DrinkVoucherUsedView />
       {/* <DrinkVoucherView /> */}
+      <StatusBar style="light" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
