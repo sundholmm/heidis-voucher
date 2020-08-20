0f2aed47ea31a2984381d67c3306e0ea073d678b
sundholmm <oskar.sundholm@protonmail.com>
2020-08-20 20:18:40 +0300

    made slider from PanResponder and Animated APIs

--- components/DrinkVoucherView/DrinkVoucherView.js
+++ components/DrinkVoucherView/DrinkVoucherView.js
@@ -1,10 +1,49 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, View, Animated, PanResponder, Image } from "react-native";
 
 const DrinkVoucherView = (props) => {
+  const { setUsed } = props;
+  const pan = new Animated.Value(-100);
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset(pan._value);
+      },
+      onPanResponderMove: Animated.event([null, { dx: pan }]),
+      onPanResponderRelease: () => {
+        if (pan._value >= 200) {
+          setUsed(true);
+        } else {
+          pan.setValue(-100);
+        }
+      },
+    })
+  ).current;
+
   return (
     <View style={styles.container}>
-      <View></View>
+      <Animated.View
+        style={{
+          transform: [
+            {
+              translateX: pan.interpolate({
+                inputRange: [-100, 100],
+                outputRange: [-100, 100],
+                extrapolateLeft: "clamp",
+                extrapolateRight: "clamp",
+              }),
+            },
+          ],
+        }}
+        {...panResponder.panHandlers}
+      >
+        <Image
+          style={styles.draggableImage}
+          source={require("../../assets/nyx_logo.png")}
+        />
+      </Animated.View>
     </View>
   );
 };
@@ -14,6 +53,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  draggableImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 500,
   },
 });
 
