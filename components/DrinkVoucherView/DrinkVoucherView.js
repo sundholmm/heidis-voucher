d50d1cdb83806d8ee28436b592593f05efcef138
sundholmm <oskar.sundholm@protonmail.com>
2020-08-20 17:37:33 +0300

    removed slider because thumb icon does not work smh

--- components/DrinkVoucherView/DrinkVoucherView.js
+++ components/DrinkVoucherView/DrinkVoucherView.js
@@ -1,18 +1,10 @@
 import React from "react";
-import Slider from "react-native-slider";
 import { StyleSheet, View } from "react-native";
 
 const DrinkVoucherView = (props) => {
-  const { setUsed } = props;
   return (
     <View style={styles.container}>
-      <View style={styles.slider}>
-        <Slider
-          onSlidingComplete={() => setUsed(true)}
-          thumbImage={require("../../assets/nyx_logo.png")}
-          thumbStyle={styles.thumb}
-        />
-      </View>
+      <View></View>
     </View>
   );
 };
@@ -22,17 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  slider: {
-    width: 150,
-  },
-  thumb: {
-    // width: 30,
-    // height: 30,
-    // shadowColor: "black",
-    // shadowOffset: { width: 0, height: 1 },
-    // shadowOpacity: 0.5,
-    // shadowRadius: 1,
   },
 });
 
